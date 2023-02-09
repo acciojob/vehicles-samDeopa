a49d08486138f63da92767cfde018fb61bdb465e
samdeopa <you@example.com>
2023-02-09 23:05:03 +0530

    added a constructor in the boat class

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,8 +2,14 @@ package com.driver;
 
 public class Boat implements WaterVehicle {
 
-    private String name="Sam";
-    private int capacity=100 ;
+    private String name;
+    private int capacity ;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
